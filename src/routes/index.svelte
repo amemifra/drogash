f738491042cd201a3dad2436934ef4da91d6eca5
Francesco Reali <f.reali@outlook.com>
2019-10-12 18:33:12 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,49 @@
+<style>
+	h1, h3, a, figure {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	img {
+		margin-top: 2em;
+		max-width: 80vw;
+		width: 50vh;
+		filter: gray;
+		-webkit-filter:  saturate(2) drop-shadow(1) brightness(3); 
+		border-radius: 100%;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 1em 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	figcaption {
+		margin-top: 2em;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Drogash • Il sogno</title>
+</svelte:head>
+
+<h1>Drogash</h1>
+<h3>Il sogno</h3>
+<figure>
+	<img alt='Napoleone Bonaparte prima della battaglia di Austerliz' src='napoleone.jpg'>
+	<figcaption>Napoleone Bonaparte prima della battaglia di Austerliz</figcaption>
+</figure>
+
+<a href="ipfs/QmQLjja4LfZfAkP8eWbx4RRAGjHe6wtqTLTMEeLNwhbUy1">Ministri in carica</a>
